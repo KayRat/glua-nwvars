2c51c3c176038eafb92b3372623dd5da84690855
KayRat <kyle.ratti@outlook.com>
2015-12-01 20:35:31 -0500

    Fixed tabbing, added method aliases

--- lua/autorun/sh_nwvars.lua
+++ lua/autorun/sh_nwvars.lua
@@ -1,103 +1,109 @@
 nwvars = nwvars or {}
 nwvars.data = nwvars.data or {}
 nwvars.privacy = {
-    SHARED,
-    PRIVATE,
+  SHARED,
+  PRIVATE,
 }
 local meta = FindMetaTable("Player") -- TODO: support all entities, not just players
 
 if(SERVER) then
-    util.AddNetworkString("nwvars.set")
-    util.AddNetworkString("nwvars.remove")
-    util.AddNetworkString("nwvars.fullUpdate")
+  util.AddNetworkString("nwvars.set")
+  util.AddNetworkString("nwvars.remove")
+  util.AddNetworkString("nwvars.fullUpdate")
 end
 
 function meta:getNWVar(strVar, objDefault)
-    local strUniqueID = self:UniqueID()
+  local strUniqueID = self:UniqueID()
 
-    if(nwvars.data[strUniqueID] and nwvars.data[strUniqueID][strVar] ~= nil) then
-        return SERVER and nwvars.data[strUniqueID][strVar]["data"] or nwvars.data[strUniqueID][strVar]
-    end
+  if(nwvars.data[strUniqueID] and nwvars.data[strUniqueID][strVar] ~= nil) then
+      return SERVER and nwvars.data[strUniqueID][strVar]["data"] or nwvars.data[strUniqueID][strVar]
+  end
 
-    return objDefault
+  return objDefault
 end
 
+meta.GetNWVar = meta.getNWVar
+
 if(SERVER) then
-    function meta:setNWVar(strVar, objData, iPrivacy)
-        iPrivacy = iPrivacy or nwvars.privacy.PRIVATE
+  function meta:setNWVar(strVar, objData, iPrivacy)
+    iPrivacy = iPrivacy or nwvars.privacy.PRIVATE
 
-        local strUniqueID = self:UniqueID()
+    local strUniqueID = self:UniqueID()
 
-        nwvars.data[strUniqueID] = nwvars.data[strUniqueID] or {}
+    nwvars.data[strUniqueID] = nwvars.data[strUniqueID] or {}
 
-        nwvars.data[strUniqueID][strVar] = {
-            ["name"] = strVar,
-            ["data"] = objData,
-            ["privacy"] = iPrivacy,
-        }
+    nwvars.data[strUniqueID][strVar] = {
+        ["name"] = strVar,
+        ["data"] = objData,
+        ["privacy"] = iPrivacy,
+    }
 
-        net.Start("nwvars.set")
-            net.WriteString(strUniqueID)
-            net.WriteString(strVar)
-            net.WriteType(objData)
+    net.Start("nwvars.set")
+        net.WriteString(strUniqueID)
+        net.WriteString(strVar)
+        net.WriteType(objData)
 
-        if(iPrivacy == nwvars.privacy.SHARED) then
-            net.Broadcast()
-        else
-            net.Send(self)
-        end
-    end
-
-    function meta:removeNWVar(strVar)
-        local strUniqueID = self:UniqueID()
-
-        if(nwvars.data[strUniqueID]) then
-            nwvars.data[strUniqueID][strVar] = nil
-        end
-
-        net.Start("nwvars.remove")
-            net.WriteString(strUniqueID)
-            net.WriteString(strVar)
+    if(iPrivacy == nwvars.privacy.SHARED) then
         net.Broadcast()
+    else
+        net.Send(self)
+    end
+  end
+
+  function meta:removeNWVar(strVar)
+    local strUniqueID = self:UniqueID()
+
+    if(nwvars.data[strUniqueID]) then
+        nwvars.data[strUniqueID][strVar] = nil
     end
 
-	local function clearVars(strUniqueID)
-		if(nwvars.data[strUniqueID]) then
-            nwvars.data[strUniqueID] = nil
-        end
+    net.Start("nwvars.remove")
+      net.WriteString(strUniqueID)
+      net.WriteString(strVar)
+    net.Broadcast()
+  end
 
-        net.Start("nwvars.clear")
-            net.WriteString(strUniqueID)
-        net.Broadcast()
-	end
-
-    function meta:clearNWVars()
-        clearVars(self:UniqueID())
+  local function clearVars(strUniqueID)
+    if(nwvars.data[strUniqueID]) then
+      nwvars.data[strUniqueID] = nil
     end
 
-	hook.Add("PlayerInitialSpawn", "nwvars.sendUpdate", function(objPl)
-		local tblVars = {}
+    net.Start("nwvars.clear")
+      net.WriteString(strUniqueID)
+    net.Broadcast()
+  end
 
-		for strUniqueID,objData in pairs(nwvars.data) do
-			tblVars[strUniqueID] = {}
+  function meta:clearNWVars()
+    clearVars(self:UniqueID())
+  end
 
-			for strVar,objVarData in pairs(objData) do
-				tblVars[strUniqueID][strVar] = objVarData["data"]
-			end
-		end
+  meta.SetNWVar = meta.setNWVar
+  meta.RemoveNWVar = meta.removeNWVar
+  meta.ClearNWVars = meta.clearNWVars
 
-		net.Start("nwvars.fullUpdate")
-			net.WriteTable(tblVars)
-		net.Send(objPl)
-	end)
+  hook.Add("PlayerInitialSpawn", "nwvars.sendUpdate", function(objPl)
+    local tblVars = {}
 
-	hook.Add("PlayerDisconnected", "nwvar.cleanup", function(objPl)
-		local strUniqueID = objPl:UniqueID()
+    for strUniqueID,objData in pairs(nwvars.data) do
+      tblVars[strUniqueID] = {}
 
-		timer.Start(0, function()
-			clearVars(strUniqueID)
-		end)
-	end)
+      for strVar,objVarData in pairs(objData) do
+        tblVars[strUniqueID][strVar] = objVarData["data"]
+      end
+    end
+
+    net.Start("nwvars.fullUpdate")
+      net.WriteTable(tblVars)
+    net.Send(objPl)
+  end)
+
+  hook.Add("PlayerDisconnected", "nwvar.cleanup", function(objPl)
+    local strUniqueID = objPl:UniqueID()
+
+    timer.Start(0, function()
+      clearVars(strUniqueID)
+    end)
+  end)
 end
 
 if(CLIENT) then
@@ -119,15 +125,15 @@ if(CLIENT) then
         end
     end)
 
-	net.Receive("nwvars.fullUpdate", function(iLen)
-		local tblVars = net.ReadTable()
+  net.Receive("nwvars.fullUpdate", function(iLen)
+    local tblVars = net.ReadTable()
 
-		for strUniqueID,tblData in pairs(tblVars) do
-			nwvars.data[strUniqueID] = nwvars.data[strUniqueID] or {}
+    for strUniqueID,tblData in pairs(tblVars) do
+      nwvars.data[strUniqueID] = nwvars.data[strUniqueID] or {}
 
-			for strVar,objData in pairs(tblData) do
-				nwvars.data[strUniqueID][strVar] = objData
-			end
-		end
-	end)
+      for strVar,objData in pairs(tblData) do
+        nwvars.data[strUniqueID][strVar] = objData
+      end
+    end
+  end)
 end
